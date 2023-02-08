ec8f5d3c9e81c6893cebae4e700f17b389a1e147
Gino <gagf50@gmail.com>
2023-02-08 16:24:53 -0500

    latest build

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,16 +6,52 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "Audit this contract to find all vulnerabilities in security. Also nummerate all suggestions on how to address these vulnerabilities, give both a simple explanation easy to understand and also give a code exaple on how to fix it based on the code povided, without saying simple or code, give just the code they need to add or substract. Use this format for the output: Vulnerabilities:<br/>'state the vunerabilities 'Suggestions: 'the simple explanation' Example: 'the code example' This is the contract:";
+const basePromptPrefix = `
+    Audit this contract to find all vulnerabilities in security.
+    Also, enumerate all suggestions on how to address these vulnerabilities, 
+    give both a detailed explanation, easy to understand, of what the exposure is in that case, and also provide a code example on how to fix it based on the contract provided.
+    Give just the code they need to add or subtract, and mention the number of the line as a reference, if applicable. 
+    Use this format for the output: 
+    Vulnerabilities: (make this bold)
+
+    1. 'state the vunerability 1'
+
+    2. 'state the vunerability 2'
+
+    3. '...'
+
+    
+    Suggestions: (make this bold)
+ 
+    1. 'explanation of vulnerability 1'
+
+    Example:
+    'reference number of line for the vulnerability 1'
+    'the code example' 
+
+    2. 'explanation of vulnerability 2'
+
+    Example:
+    'reference number of line for the vulnerability 2'
+    'the code example' 
+
+    3. '...'
+
+    Example:
+    '...'
+    '...' 
+    
+    This is the contract:
+    `;
 const generateAction = async (req, res) => {
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 250,
+    temperature: 0.6,
+    max_tokens: 1000,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
